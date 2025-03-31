4e24aaf7b14221d75a53066bc5e9b70052e37a37
Ivan <149313295+xiaRuen@users.noreply.github.com>
2025-03-31 01:54:06 +0100

    Added Bush, Butterfly and UI 0.5 implementation

--- Assets/Scripts/UI/StartMenuUI.cs
+++ Assets/Scripts/UI/StartMenuUI.cs
@@ -11,15 +11,21 @@ public class StartMenuUI : MonoBehaviour
         var root = GetComponent<UIDocument>().rootVisualElement;
         panels = new VisualElement[] {
             root.Q<VisualElement>("StartPanel"),
-            root.Q<VisualElement>("SettingsPanel"),
+            root.Q<VisualElement>("LevelsPanel"),
             root.Q<VisualElement>("AboutPanel")
         };
-        root.Q<Button>("Start").clicked += () => { SceneManager.LoadScene("Game Base"); SceneManager.LoadSceneAsync("1", LoadSceneMode.Additive); };
-        root.Q<Button>("Settings").clicked += () =>PanelToggle(1);
+        root.Q<Button>("Start").clicked += () => StartGame("1");
+        root.Q<Button>("Levels").clicked += () => PanelToggle(1);
         root.Q<Button>("About").clicked += () => PanelToggle(2);
-        root.Q<Button>("CloseSettings").clicked += () => PanelToggle(0);
+        root.Q<Button>("CloseLevels").clicked += () => PanelToggle(0);
         root.Q<Button>("CloseAbout").clicked += () => PanelToggle(0);
         root.Q<Button>("Exit").clicked += () => { Application.Quit(); };
+
+        for (int i = 0; i < 3; i++)
+        {
+            string sceneName = (i + 1).ToString();
+            root.Q<Button>($"l{i + 1}").clicked += () => StartGame(sceneName);
+        }
     }
 
     void PanelToggle(int panelIndex)
@@ -29,6 +35,13 @@ public class StartMenuUI : MonoBehaviour
             panels[i].style.flexGrow = i == panelIndex ? 1 : 0;
         }
 
+    }
+
+    void StartGame(string sceneName)
+    {
+        Debug.Log($"Starting game with scene: {sceneName}");
+        SceneManager.LoadScene("Game Base");
+        SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
     }
 
 }
